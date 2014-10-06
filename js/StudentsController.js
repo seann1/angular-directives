1b5a301c7348e9197b36e60f32c649a3f67cd23c
Sean <sean@seans-mbp.metaltoad.net>
2014-10-06 14:03:21 -0700

    add hobbies

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,7 +1,9 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
   $scope.students = [];
+  $scope.hobbies = [];
   $scope.addStudent = function() {
     $scope.students.push({"name": $scope.studentName});
+    $scope.hobbies.push({"hobby": $scope.studentHobby});
     $scope.studentName = null;
   };
 });
