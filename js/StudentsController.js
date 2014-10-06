e9e513a9753dfa13511197d717987cdebe23434a
Sean <sean@seans-mbp.metaltoad.net>
2014-10-06 13:40:02 -0700

    add angular directive

---
+++ js/StudentsController.js
@@ -0,0 +1,7 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({"name": $scope.studentName});
+    $scope.studentName = null;
+  };
+});
